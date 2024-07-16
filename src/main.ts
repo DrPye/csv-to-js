56ecfbfc87e4f9de6737e7b31dbe8b7291d1ecf6
Tom Pye <13236011+DrPye@users.noreply.github.com>
2024-07-16 11:44:59 +0100

    feat: init setup of tool

---
+++ src/main.ts
@@ -0,0 +1,57 @@
+// Set the inner HTML first
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+   <form id="csvForm">
+      <input type="file" id="csvInput" accept=".csv" />
+      <input type="submit" value="Submit" />
+    </form>
+    <textarea name="" id="csvResult" cols="30" rows="10"></textarea>
+`;
+
+// Now that the elements exist in the DOM, access them
+import "./style.css";
+const form = document.querySelector("#csvForm")!;
+const csvFileInput = document.querySelector("#csvInput")! as HTMLInputElement;
+const textArea = document.querySelector("#csvResult")! as HTMLTextAreaElement;
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const file = csvFileInput.files && csvFileInput.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    const csvArray = csvToArr((e.target as FileReader).result as string, ",");
+    console.log("textarea: ", textArea);
+    textArea.value = JSON.stringify(csvArray, null, 4);
+  };
+
+  if (file) {
+    reader.readAsText(file);
+  }
+});
+
+interface CSVObject {
+  [key: string]: string;
+}
+
+function csvToArr(stringVal: string, splitter: string): CSVObject[] {
+  // Normalize line endings and then split into lines
+  const lines = stringVal
+    .trim()
+    .replace(/\r\n/g, "\n")
+    .replace(/\r/g, "\n")
+    .split("\n");
+
+  // Split each line by the splitter to separate keys from values
+  const [keys, ...rest] = lines.map((item) => item.split(splitter));
+
+  // Map the rest of the lines to objects using keys
+  const formedArr = rest.map((item) => {
+    const object: CSVObject = {};
+    keys.forEach((key, index) => {
+      object[key] = item[index];
+    });
+    return object;
+  });
+
+  return formedArr;
+}
