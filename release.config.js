0e3d50088dbb1fca66ed76f4eddff78b982ea60f
Tom Pye <13236011+DrPye@users.noreply.github.com>
2024-07-18 15:33:07 +0100

    ci: add release config for semantic release
    fix #4

---
+++ release.config.js
@@ -0,0 +1,11 @@
+const config = {
+    branches: ['main'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/github',
+    ],
+  };
+  
+  module.exports = config;
+  
